7f95a6b6814c0958ec7e04f1dceb594b5f449ffa
silvasbr <98825880+silvasbr@users.noreply.github.com>
2026-01-07 23:46:59 -0300

    Delete services directory

--- services/services/metaService.ts
+++
@@ -1,8 +0,0 @@
-
-export const metaService = {
-  getAccountStatus: async () => ({ connected: !!localStorage.getItem('meta_token') }),
-  publishToInstagram: async (url: string, caption: string) => {
-    console.log('Publicando...', { url, caption });
-    return { success: true };
-  }
-};
