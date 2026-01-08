622879b585ee50443dcc2c96af2775f52915497e
silvasbr <98825880+silvasbr@users.noreply.github.com>
2026-01-07 23:35:13 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// @ts-ignore
-const envApiKey = typeof process !== 'undefined' ? process.env.API_KEY : '';
-
 export default defineConfig({
   plugins: [react()],
   define: {
-    'process.env.API_KEY': JSON.stringify(envApiKey)
+    'process.env.API_KEY': JSON.stringify(process.env.API_KEY || '')
   }
 });
